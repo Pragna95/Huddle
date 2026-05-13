33305a4ee0006a82b5bea33ad2c845b6f46dea05
Sharmilashaik1012 <sharmilashaik1012@gmail.com>
2026-05-13 19:18:13 +0530

    Initial modular backend architecture setup

---
+++ backend/modules/audit_logs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuditLogsConfig(AppConfig):
+    name = 'modules.audit_logs'
---
+++ backend/modules/meeting_sessions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SessionsConfig(AppConfig):
+    name = 'modules.meeting_sessions'
